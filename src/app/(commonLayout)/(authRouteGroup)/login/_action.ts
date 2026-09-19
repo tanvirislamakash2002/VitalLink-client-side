54b37484a256fc9725f17fd4da78f6a91de40086
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-09-19 20:17:01 +0600

    feat: enhance login action to set tokens in cookies and redirect to dashboard

--- src/app/(commonLayout)/(authRouteGroup)/login/_action.ts
+++ src/app/(commonLayout)/(authRouteGroup)/login/_action.ts
@@ -2,9 +2,11 @@
 "use server"
 
 import { httpClient } from "@/src/lib/axios/httpClient";
+import { setTokenInCookies } from "@/src/lib/tokenUtils";
 import { ApiErrorResponse } from "@/src/types/api.types";
 import { ILoginResponse } from "@/src/types/auth.types";
 import { ILoginPayload, loginZodSchema } from "@/src/zod/auth.validation";
+import { redirect } from "next/navigation";
 
 export const loginAction = async (payload: ILoginPayload): Promise<ILoginResponse | ApiErrorResponse> => {
     const parsedPayload = loginZodSchema.safeParse(payload)
@@ -17,8 +19,15 @@ export const loginAction = async (payload: ILoginPayload): Promise<ILoginRespons
     }
     try {
         const response = await httpClient.post<ILoginResponse>("/auth/login", parsedPayload.data)
-        const { accessToken, refreshToken, token, user } = response.data;
-        return response.data
+
+        const { accessToken, refreshToken, token } = response.data;
+
+        await setTokenInCookies("accessToken", accessToken)
+        await setTokenInCookies("refreshToken", refreshToken)
+        await setTokenInCookies("better-auth.session_token", token)
+
+        redirect("/dashboard")
+
     } catch (error: any) {
         return {
             success: false,
