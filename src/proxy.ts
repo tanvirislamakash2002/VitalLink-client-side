ea91e82f1820f8378a6071589abdf37954252aef
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-09-21 19:34:52 +0600

    feat: implement proxy middleware with JWT verification and role-based redirection

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,37 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { jwtUtils } from "./lib/jwtUtils";
+import { getDefaultDashboardRoute, getRouteOwner, isAuthRoute, UserRole } from "./lib/authUtils";
 
 export async function proxy(request: NextRequest) {
+    try {
+        const { pathname } = request.nextUrl;
+        const accessToken = request.cookies.get("accessToken")?.value;
+        const refreshToken = request.cookies.get("refreshToken")?.value;
 
+        const decodedAccessToken = accessToken && jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string).data;
+
+        const isValidAccessToken = accessToken && jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string).success
+
+        let userRole: UserRole | null = null
+
+        if (decodedAccessToken) {
+            userRole = decodedAccessToken.role as UserRole;
+        }
+
+        const routerOwner = getRouteOwner(pathname)
+        const unifySuperAdminAndAdminRole = userRole === "SUPER_ADMIN" ? "ADMIN" : userRole;
+        userRole = unifySuperAdminAndAdminRole
+
+        const isAuth = isAuthRoute(pathname);
+
+        if (isAuth && isValidAccessToken) {
+            return NextResponse.redirect(new URL(getDefaultDashboardRoute(userRole as UserRole), request.url))
+        }
+
+        return NextResponse.next()
+    } catch (error) {
+        console.log("Error in proxy middleware:", error)
+    }
 }
 
 export const config = {
