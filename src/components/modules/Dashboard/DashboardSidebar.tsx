ea403882dcd8ab2c39217a36d5502bcd6bac0db6
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-09-24 11:43:05 +0600

    feat: implement dynamic navigation based on user role with sidebar integration

--- src/components/modules/Dashboard/DashboardSidebar.tsx
+++ src/components/modules/Dashboard/DashboardSidebar.tsx
@@ -1,9 +1,18 @@
+import { getDefaultDashboardRoute } from '@/src/lib/authUtils';
+import { getNavItemsByRole } from '@/src/lib/navItems';
+import { getUserInfo } from '@/src/services/auth.services';
+import { NavSection } from '@/src/types/dashboard.types';
 import React from 'react';
 
-const DashboardSidebar = () => {
+const DashboardSidebar = async () => {
+    const userInfo = await getUserInfo()
+    const navItems: NavSection[] = getNavItemsByRole(userInfo.role)
+    
+    const dashbaordHome = getDefaultDashboardRoute(userInfo.role)
+
     return (
         <div>
-            
+
         </div>
     );
 };
