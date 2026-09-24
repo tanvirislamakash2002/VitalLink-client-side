1a313c3e916a674f1ddeb3899bb0067f0258e2fb
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-09-24 14:11:12 +0600

    feat: implement ScrollArea component and integrate it into DashboardSidebarContent for improved navigation

--- src/components/modules/Dashboard/DashboardSidebarContent.tsx
+++ src/components/modules/Dashboard/DashboardSidebarContent.tsx
@@ -1,8 +1,12 @@
 "use client"
 
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { NavSection } from '@/src/types/dashboard.types';
 import { UserInfo } from '@/src/types/user.types';
+import { cn } from 'cn';
+import { Home } from 'lucide-react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 interface DashboardSidebarContentProps {
@@ -12,7 +16,7 @@ interface DashboardSidebarContentProps {
 }
 
 const DashboardSidebarContent = ({ dashboardHome, navItems, userInfo }: DashboardSidebarContentProps) => {
-
+    const pathname = usePathname()
     return (
 
         <div className='hidden md:flex h-full w-64 flex-col border-r bg-card'>
@@ -24,7 +28,46 @@ const DashboardSidebarContent = ({ dashboardHome, navItems, userInfo }: Dashboar
             </div>
 
             {/* Navigation Area */}
+            <ScrollArea className="flex-1 px-3 py-4">
+                <nav className='space-y-6'>
+                    {
+                        navItems.map((section, sectionId) => (
+                            <div key={sectionId}>
+                                {
+                                    section.title && (
+                                        <h4 className='mb-2 px-3 text-xs font-semibold text-muted-foreground uppercase tracking-wider'>{section.title}</h4>
+                                    )
+                                }
+                                <div className='space-y-1'>
+                                    {
+                                        section.items.map((item, id) => {
+                                            const isActive = pathname === item.href
+                                            // const Icon = <Home />
 
+                                            return (
+                                                <Link
+                                                    href={item.href}
+                                                    key={id}
+                                                    className={
+                                                        cn("flex items-center gap-3 rounded-lg px-3 py-2 text-sm font-medium transition-all",
+                                                            isActive ?
+                                                                "bg-primary text-primary-foreground" :
+                                                                "text-muted-foreground hover:bg-accent hover:text-accent-foreground"
+                                                        )
+                                                    }
+                                                >
+                                                    <Home />
+                                                    <span>{item.title}</span>
+                                                </Link>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        ))
+                    }
+                </nav>
+            </ScrollArea>
             {/* User Info At Bottom */}
             <div className='border-t px-3 py-4'>
                 <div className='flex items-center gap-3'>
