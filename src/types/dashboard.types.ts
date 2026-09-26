56b0a3b157bb576df06bdecd6cd3264c00742217
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-09-26 19:26:10 +0600

    feat: add AppointmentPieChart component and update dashboard types with additional data fields

--- src/types/dashboard.types.ts
+++ src/types/dashboard.types.ts
@@ -1,10 +1,33 @@
 export interface NavItem {
-    title: string,
-    href: string,
-    icon: string,
+    title : string,
+    href : string,
+    icon : string
 }
 
 export interface NavSection {
-    title?: string,
-    items: NavItem[]
+    title ?: string,
+    items : NavItem[]
+}
+
+export interface PieChartData {
+    status: string,
+    count: number
+}
+
+export interface BarChartData {
+    month: Date | string,
+    count: number
+}
+
+export interface IAdminDashboardData {
+    appointmentCount : number;
+    patientCount : number;
+    doctorCount : number;
+    adminCount : number;
+    superAdminCount : number;
+    paymentCount : number;
+    userCount : number;
+    totalRevenue : number;
+    barChartData : BarChartData[];
+    pieChartData : PieChartData[];
 }
